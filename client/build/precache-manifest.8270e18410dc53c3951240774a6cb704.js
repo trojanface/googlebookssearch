3c952b307c1c9d2e0f7fccab4c9946775d3bc839
MattT <trojanface@hotmail.com>
2020-07-20 19:44:26 +0930

    Pushed build folder

---
+++ client/build/precache-manifest.8270e18410dc53c3951240774a6cb704.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e546afe8061ca9a16d8d9c427e8df8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d93e31a7009c7aa6e045",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b730cfd44a5b12e6caa",
+    "url": "/static/js/2.3aef1ee2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3aef1ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93e31a7009c7aa6e045",
+    "url": "/static/js/main.2898a382.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
